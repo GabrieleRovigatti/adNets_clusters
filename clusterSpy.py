96487ede3b18e3ea96d7e64957d68f6b73bd7281
Gabriele Rovigatti <gabriele.rovigatti@gmail.com>
2019-05-09 19:08:24 +0200

    Add files via upload

--- clusterSpy.py
+++ clusterSpy.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # ClusterSpy.py: script to inspect illustrative (randomly chosen) clusters 
 # from the paper "From Mad Men to Maths Men:  Concentration and Buyer Power in Online Advertising"
 # by Francesco Decarolis and Gabriele Rovigatti
@@ -27,12 +28,12 @@ def readClust(data):
     while ind.isdigit() == False or int(ind) > 23 or int(ind) < 1:
         ind = input('Please, input numbers 1 to 23 only!')
     inddata = data.loc[int(ind)] # subset the industry
-    inddata.set_index('cluster', inplace=True) # reset the index to cluster
+    inddata.set_index('cluster_ID', inplace=True) # reset the index to cluster
     return inddata['keyword'].loc[randint(1, 6)] # return a random cluster
 # Main Code #
 if __name__ == "__main__":
     clustdir = dirname(abspath(__file__)) + '/cluster_examples_full.csv'
-    cldf = pd.read_csv(clustdir, index_col = 0, delimiter = '\t')
+    cldf = pd.read_csv(clustdir, index_col = 3, delimiter = '\t')
     reader = 'Y'
     while reader == 'Y' or reader == 'y' or reader == 'Yes':
         print(readClust(cldf).to_frame().to_string(index=False, justify = 'initial'))
